66d7c825efd3252d80526883ff79a12523678d37
Katia Villevald <kvillevald@gmail.com>
2016-04-18 00:18:45 -0500

    Added files via upload

---
+++ CodeCafe2.0/PostPage.css
@@ -0,0 +1,149 @@
+body{
+	background: url("http://images4.alphacoders.com/888/8.jpg") no-repeat center center fixed;
+	background-size: cover;
+	font-family: "Roboto", helvetica, arial, sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  text-rendering: optimizeLegibility;
+}
+
+.nav a {
+  color: #FFE4C4;
+  font-size: 10px;
+  font-weight: bold;
+  font-size: 25px;
+  text-decoration: none;
+}
+.nav li {
+    display:inline;
+}
+
+.centered-pills { text-align:center; }
+.centered-pills ul.nav-pills { display:inline-block; }
+.centered-pills li { display:inline; }
+.centered-pills a { float:left; }
+* html .centered-pills ul.nav-pills { display:inline; } /* IE6 */
+*+html .centered-pills ul.nav-pills { display:inline; } /* IE7 */
+
+#posts{
+	
+	display:inline-block;
+	vertical-align: top;
+	margin-left:180px;
+	margin-right:0px;
+	margin-top:60px;
+	margin-bottom: 0px;
+
+	width:300px;
+	height:500px;
+	background: url("http://i.stack.imgur.com/YEwNY.png") no-repeat center;
+  background-size: contain;
+  
+  
+
+
+	
+}
+
+#posts ul{
+height:360px; 
+width:100%;
+font-size: 15px;
+
+}
+#posts ul{overflow:hidden; overflow-y:scroll;}
+
+#display{
+	width:350px;
+	height:550px;
+	display:inline-block;
+	margin-left:0px;
+	margin-top:60px;
+	padding-bottom: 10px;
+	color: #FFE4C4;
+	margin-bottom: 10px;
+	overflow-y:scroll; 
+	
+
+
+}
+#container {
+
+	
+	margin-top: 50px;
+	margin-left: 100px;
+	padding-left:50px;
+	height: 670px;
+	width: 440px;
+	display:inline-block;
+  
+  
+  margin-left: 0px auto;
+  background: url("https://lh3.googleusercontent.com/Mh8xpAaNWyVhLnep3RebPpIKdEKtPK7D7xnsylvub4boNanPAvb36qhExCz-QkHC51Zg-g=w1342-h503 ") no-repeat center;
+  background-size: contain;
+  box-shadow: 10px 10px 5px #888888;
+}
+
+
+#pTitle{
+	color:#3a2925;
+	align:right;
+	font-size: 30px;
+	font-family: "Courier New";
+	font-style: bold;
+	font-weight: 900;
+}
+#Name{
+	color:black;
+	font-weight: 600;
+
+font-family: "Courier New";
+}
+
+#image{
+	border-style: solid;
+	border-width: 20px;
+	border-color: #999999;
+
+}
+#des{
+	background-color: #323232;
+	color:#e5e5e5;
+	font-family: "Courier New";
+	font-weight: 600;
+	height:200px;
+	width:300px;
+	
+	margin-top: 30px;
+	margin-bottom: 20px;
+	
+}
+div.table-title {
+   display: block;
+  margin-left:40px;
+  max-width: 600px;
+  padding:5px;
+  width: 100%;
+}
+
+.table-title h3 {
+   color: #fafafa;
+   font-size: 27px;
+   font-weight: 400;
+   font-style:normal;
+   font-family: "Roboto", helvetica, arial, sans-serif;
+   text-shadow: -1px -1px 1px rgba(0, 0, 0, 0.1);
+   text-transform:uppercase;
+}
+
+.cup{ 
+	height:300px;
+	width:300px;
+	background: url('http://pngimg.com/upload/small/cup_PNG1995.png')no-repeat center;
+	background-size: contain;
+	margin-top: 150px;
+	margin-left:900px;
+
+}
+
+
---
+++ CodeCafe2.0/PostPage.js
@@ -0,0 +1,210 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var URL = 'https://docs.google.com/spreadsheets/d/1yEpL6bAdPt5bWG3vpOKVBD6O3xOwqshd9M0HmDLzps0/pubhtml';
+    Tabletop.init( { key: URL, callback: makeUL, simpleSheet: true } )
+   
+  //  Tabletop.init( { key: URL, callback: displayShow, simpleSheet: true } )
+   
+    
+})
+
+/*function printResult(data){
+	var ProjectTitles= [];
+	var fileUploads = [];
+	var Name = [];
+	var Description = [];
+	var out = "";
+	for(var i=0; i< data.length; i++) {
+		alert(i);
+		var postID="post"+i;
+		document.getElementById(postID).innerHTML= data[i].ProjectTitle;
+     
+		ProjectTitles[i]=data[i].ProjectTitle;
+		
+		
+		
+}
+}
+
+*/
+
+/*
+function removePost(){
+
+    document.getElementsByTagName('H1'). innerHTML= 'none';
+    
+
+}
+
+
+function displayShow(id){
+
+    
+    var h= document.createElement("H1");
+    h.id="header";
+    var t = document.createTextNode(document.getElementById(id).textContent);
+    var title=h.appendChild(t);
+    document.getElementById("display").appendChild(title);
+    return h;
+    
+   
+
+	 
+	
+	
+
+}
+
+
+function makeOtherElements(data,id){
+    console.log(data);
+    alert(id);
+    var idNew =String(id);
+    var i = idNew.split("_").pop();
+    var p= document.createElement("P");
+    p.id="name";
+    var t = document.createTextNode(data[i].YourNameFirstandLast);
+    var v=p.appendChild(t);
+    document.getElementById("display").appendChild(v);
+}
+*/
+
+
+function makeUL(data) {
+    // Create the list element:
+    var list = document.createElement('ul');
+    
+    
+
+
+    for(var i = 0; i < data.length; i++) {
+        // Create the div item:
+        var div = document.createElement('div');
+        div.style.width = '200px';
+        div.style.height='50px';
+        div.style.border="3px solid #C1C3D1";
+        div.style.color = "#666B85";
+        div.style.background = "white";
+        div.style.textAlign="center";
+        div.style.padding="-1px 2px 10px";
+        div.style.margin = "0px 0px 0px 10px";
+        
+
+
+
+        //create list item
+        var a = document.createElement('a');
+        var item = document.createElement('li');
+        
+        
+        a.setAttribute('href', '#display');
+        
+        item.appendChild(a);
+        div.appendChild(item);
+        div.setAttribute('id', 'post_'+i);
+        
+
+        // Add it to the list:
+
+        list.appendChild(div);
+        
+        document.getElementById('posts').appendChild(list); 
+        document.getElementById('post_'+i).textContent = data[i].ProjectTitle;
+    }
+
+
+    var oldID='post_';
+    list.addEventListener('click', function(e) {
+    
+   
+    var target = e.target; // Clicked element
+    
+    while (target && target.parentNode !== list) {
+        target = target.parentNode;
+         // If the clicked element isn't a direct child
+        if(!target) { alert("NO");return; } // If element doesn't exist
+    }
+
+    if (target.tagName === 'DIV'){
+        alert("yes");
+        alert(target.id);
+
+        if(oldID!='post_'){
+            document.getElementById('pTitle').innerHTML = '';
+            document.getElementById('image').innerHTML = '';
+            document.getElementById('Name').innerHTML = '';
+            document.getElementById('des').innerHTML = '';
+            
+        }
+        
+        //display title
+
+        var h = document.createElement('h1');
+
+        var t = document.createTextNode(document.getElementById(target.id).textContent);
+        var title=h.appendChild(t);
+        document.getElementById('pTitle').appendChild(title);
+
+        //display other elements
+        var id = target.id;
+        console.log(data);
+        alert(id);
+        var idNew =String(id);
+        var i = idNew.split('_').pop();
+        
+        //create a break
+        var br = document.createElement('br');
+        document.getElementById('pTitle').appendChild(br);
+
+        //create the name
+        var p= document.createElement('P');
+        
+        var t = document.createTextNode(data[i].YourNameFirstandLast);
+        var v=p.appendChild(t);
+        var v = document.getElementById('Name').appendChild(v);
+        
+
+        //create a break
+        var br = document.createElement('br');
+        document.getElementById('Name').appendChild(br);
+        //create a break
+        var br = document.createElement('br');
+        document.getElementById('Name').appendChild(br);
+        //create a break
+        var br = document.createElement('br');
+        document.getElementById('Name').appendChild(br);
+
+        //create the image
+        var x = document.createElement('IMG');
+        x.setAttribute('src', data[i].File);
+        x.setAttribute('width', '100%');
+        x.setAttribute('width', '100%');
+        
+        document.getElementById('image').appendChild(x);
+
+         //create a break
+        var br = document.createElement('br');
+        document.getElementById('image').appendChild(br);
+       
+
+        //create the description
+        var d= document.createElement('P');
+        var t = document.createTextNode(data[i].Description);
+        var c=d.appendChild(t);
+        document.getElementById('des').appendChild(c);
+        
+
+
+        oldID=target.id;
+        
+
+
+    }
+
+});
+
+    
+
+    // Finally, return the constructed list:
+    ;
+}
+
